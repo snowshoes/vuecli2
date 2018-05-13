c41e3bc562403bc9ea3ca213c3674792f8e922a1
snowshoe <shaokun.wen@gmail.com>
2018-05-13 18:54:21 +0200

    add prettier eslint-plugin-prettier eslint-config-prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    // https://medium.com/@doppelmutzi/eslint-prettier-vue-workflow-46a3cf54332f
+    'prettier',
+    'prettier/standard',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/recommended'
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    // 'standard'
+  ],
+  // required to lint *.vue files
+  plugins: ['vue', 'prettier'],
+  // add your custom rules here
+  rules: {
+    // https://github.com/doppelmutzi/eslint-prettier-vue-demo/blob/master/.eslintrc.js
+    'prettier/prettier': 'error',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+};
